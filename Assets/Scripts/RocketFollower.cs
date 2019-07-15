77313808caa45caaaa3c2fdaab80656f7ccef479
jrpavoncello <jrpavoncello@gmail.com>
2019-07-15 00:17:39 -0400

    Added camera zoom out and really slow follow when the rocket explodes on collision Refactored and added tooltips

--- Assets/Scripts/RocketFollower.cs
+++ Assets/Scripts/RocketFollower.cs
@@ -3,23 +3,31 @@
 public class RocketFollower : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("Offset that will applied to the position of the camera in addition to any velocity changed.")]
     private Vector3 cameraOffset = new Vector3(0, .5f, 5.156f);
 
     [SerializeField]
     private RocketController rocket;
 
     [SerializeField]
+    [Tooltip("Multiplied to the rocket velocity in the (X, Y) direction before adding it to the offset of the camera. " +
+        "The higher the multiplier, the further ahead the camera will lead ahead of the rocket.")]
     private Vector2 velocityMultiplier = new Vector2(.1f, .2f);
 
     [SerializeField]
+    [Tooltip("Lerp rate that will be used to smoothly move the camera as the rocket is flying (not in collision).")]
     private float flightLerpRate = .7f;
 
     [SerializeField]
-    // We use a slower lerp rate when the rocket is in collision because the velocity change is really jarring
-    private float rocketCollisionLerpRate = .05f;
+    [Tooltip("Lerp rate that will be used to smoothly move the camera back to the rocket when it collides. " +
+        "This is important so that the camera doesn't hard whip back to the rocket when collision occurs.")]
+    private float collisionLerpRate = .3f;
+
+    [SerializeField]
+    [Tooltip("Lerp rate that will be used to smoothly follow the rocket as it's being exploded. Should be really low.")]
+    private float explosionLerpRate = 0.03f;
 
     private Rigidbody rocketRigidBody;
-
     private float previousLerpRate = 0f;
 
     // Start is called before the first frame update
@@ -35,24 +43,34 @@ public class RocketFollower : MonoBehaviour
 
     void FixedUpdate()
     {
+        // We do this in FixedUpdate because the camera uses RigidBody collisions to prevent it 
+        // from going through the ground. It will causes clipping and jittering otherwise.
         SetCameraPosition();
     }
 
     private void SetCameraPosition()
     {
-        var targetPosition = this.rocket.transform.position;
+        Vector3 targetPosition = this.rocket.transform.position + cameraOffset;
 
-        targetPosition += cameraOffset;
+        float lerp = rocket.IsInCollision ? collisionLerpRate : this.flightLerpRate;
 
-        var velocityOffset = new Vector3(
-            rocketRigidBody.velocity.x * velocityMultiplier.x, 
-            rocketRigidBody.velocity.y * velocityMultiplier.y, 
-            0f);
+        if (rocket.State == RocketState.Dead)
+        {
+            // Stop moving the camera and move it away on death
+            targetPosition += cameraOffset.Multiply(new Vector3(0, 0, .5f));
 
-        targetPosition += velocityOffset;
+            lerp = this.explosionLerpRate;
+        }
+        else
+        {
 
-        var lerp = rocket.IsInCollision ? rocketCollisionLerpRate : this.flightLerpRate;
+            var velocityOffset = rocketRigidBody.velocity.Multiply(new Vector3(velocityMultiplier.x, velocityMultiplier.y, 0f));
 
+            targetPosition += velocityOffset;
+        }
+
+        // Use either the new lower Lerp rate, or lerp between the old lower lerp rate and new higher lerp rate
+        // This is so collisions don't jerk the camera so hard.
         lerp = Mathf.Min(lerp, Mathf.Lerp(this.previousLerpRate, lerp, 0.001f));
 
         this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, lerp);
