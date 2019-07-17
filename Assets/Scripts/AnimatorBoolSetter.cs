45cc6dc19009b2a1f424871733f8453445ad4465
jrpavoncello <jrpavoncello@gmail.com>
2019-07-16 23:47:28 -0400

    Added a 3rd level with moving obstacles (controlled by animators)

---
+++ Assets/Scripts/AnimatorBoolSetter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorBoolSetter : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Name of the property in the animator.")]
+    private string propertyName;
+
+    [SerializeField]
+    [Tooltip("Bool value to set.")]
+    private bool value = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var animator = GetComponent<Animator>();
+        animator.SetBool(this.propertyName, this.value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/AnimatorFloatSetter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorFloatSetter : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Name of the property in the animator.")]
+    private string propertyName;
+
+    [SerializeField]
+    [Tooltip("Float value to set.")]
+    private float value = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var animator = GetComponent<Animator>();
+        animator.SetFloat(this.propertyName, this.value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
